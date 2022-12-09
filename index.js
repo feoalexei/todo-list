bdbb8f5fcf3baf83c36b8237ee929df5c91eca84
feoalexei <feoalexei@ukr.net>
2022-12-09 23:46:50 +0200

    added "add" logic

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // selectors
-const form = document.querySelector(".form");
-const input = document.querySelector(".input");
-const addBtn = document.querySelector(".add-btn");
-const taskList = document.quertSelector(".task-list");
+const form = document.querySelector('.form');
+const input = document.querySelector('.input');
+const addBtn = document.querySelector('.add-btn');
+const taskList = document.querySelector('.task-list');
 
 // event listeners
-form.addEventListener('click', addTask)
-
+addBtn.addEventListener('click', addTask);
 
 // functions
 function addTask(e) {
-    e.preventDefault();
-    taskList.insertAdjacentHTML("beforeend",taskItemMarkup()); 
+  e.preventDefault();
+  const inputValue = e.target.previousElementSibling.value;
+  taskList.insertAdjacentHTML('beforeend', taskItemMarkup(inputValue));
 }
 
-function taskItemMarkup() {
-    return `
-    <li class="item">
+function taskItemMarkup(value) {
+  return `
+    <li class="task-item">
         <input class="checkbox" type="checkbox">
-        <span class="task"></span>
-        <button class="delete-btn"></button>
-    </li>`
+        <span class="task">${value}</span>
+        <button class="delete-btn">Delete</button>
+    </li>`;
 }
